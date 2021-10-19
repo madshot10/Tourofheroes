6c88fcb4ae1fa7491ab321d36bc3c81da6dec067
madshot10 <hugompsilveira@gmail.com>
2021-10-19 11:53:38 +0100

    faltou apenas a ligaç

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
-const routes: Routes = [];
+//o routes diz ao router qual o display que deve ser inserido consoante o URL utilizado
+const routes : Routes = [
+  // quando nao ha nenhuma rota no url, este manda para o url de dashboard
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  //path é uma string que corresponde ao URL
+  //component é o component que o router deve criar (localhost:4200/heroes)
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
 
+// o (:) e um placeholder para o id, ou seja, recebe uma variavel id
+  { path: 'detail/:id', component: HeroDetailComponent},
+];
+
+//a aplicacao importa o routermodule e o router para ter uma funcionalidade de routing.
+//importa tambem o heroescomponent para dar um seguimento ao routing
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [],
+  imports: [
+    // método chama se for root porque o router é configurado para a aplicação forRoot
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+
+  @Input() hero?: Hero;
+
+  constructor(
+    // activeroute recebe a informacao da rota do HeroDetailComponent
+    private route: ActivatedRoute,
+    // recebe a informacao do respetivo hero e faz o display da mesma
+    private heroService: HeroService,
+    // display e um servico do angular que serve para interagir com o browser
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+}
